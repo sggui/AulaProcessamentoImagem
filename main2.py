1c27a10c9c21a94eb0ce095da9fcac6a646f5812
GUILHERME SANTOS GUIMARAES <guilherme.sguimarae3@senacsp.edu.br>
2024-08-20 20:42:15 -0300

    a

--- main2.py
+++ main2.py
@@ -1,14 +1,11 @@
-
-
-#pip install -v "pysimplegui==4.60.5"
-
 import PySimpleGUI as sg
-from PIL import Image
+from PIL import Image, ExifTags
 import io
 import os
 
 image_atual = None
 image_path = None
+image_metadata = {}
 
 def resize_image(img):
     img = img.resize((800, 600), Image.Resampling.LANCZOS) 
@@ -17,12 +14,26 @@ def resize_image(img):
 def open_image(filename):
     global image_atual
     global image_path
+    global image_metadata
     image_path = filename
     image_atual = Image.open(filename)    
     
+    # Extracting important metadata
+    if image_atual._getexif():
+        for k, v in image_atual._getexif().items():
+            tag_name = ExifTags.TAGS.get(k)
+            if tag_name in ['DateTime', 'Model', 'Make', 'GPSInfo']:
+                if tag_name == 'GPSInfo':
+                    gps_info = {}
+                    for t in v:
+                        sub_tag_name = ExifTags.GPSTAGS.get(t)
+                        gps_info[sub_tag_name] = v[t]
+                    image_metadata['GPSInfo'] = gps_info
+                else:
+                    image_metadata[tag_name] = v
+    
     resized_img = resize_image(image_atual)
-    #Converte a image PIL para o formato que o PySimpleGUI
-    img_bytes = io.BytesIO() #Permite criar objetos semelhantes a arquivos na memória RAM
+    img_bytes = io.BytesIO()
     resized_img.save(img_bytes, format='PNG')
     window['-IMAGE-'].update(data=img_bytes.getvalue())
 
@@ -32,26 +43,51 @@ def save_image(filename):
         with open(filename, 'wb') as file:
             image_atual.save(file)
 
+def get_gps_decimal(gps_info, ref):
+    d, m, s = gps_info
+    decimal = d + (m / 60.0) + (s / 3600.0)
+    if ref in ['S', 'W']:
+        decimal = -decimal
+    return decimal
+
 def info_image():
     global image_atual
     global image_path
+    global image_metadata
     if image_atual:
         largura, altura = image_atual.size
         formato = image_atual.format
         tamanho_bytes = os.path.getsize(image_path)
         tamanho_mb = tamanho_bytes / (1024 * 1024)
-        sg.popup(f"Tamanho: {largura} x {altura}\nFormato: {formato}\nTamanho em MB: {tamanho_mb:.2f}")
+
+        # Displaying only relevant metadata
+        model = image_metadata.get('Model', 'N/A')
+        make = image_metadata.get('Make', 'N/A')
+        date_time = image_metadata.get('DateTime', 'N/A')
+        
+        # GPS Information
+        gps_info = image_metadata.get('GPSInfo', {})
+        if gps_info:
+            lat = get_gps_decimal(gps_info.get('GPSLatitude', (0, 0, 0)), gps_info.get('GPSLatitudeRef', 'N'))
+            lon = get_gps_decimal(gps_info.get('GPSLongitude', (0, 0, 0)), gps_info.get('GPSLongitudeRef', 'E'))
+        else:
+            lat = 'N/A'
+            lon = 'N/A'
+        
+        sg.popup(f"Tamanho: {largura} x {altura}\nFormato: {formato}\nTamanho em MB: {tamanho_mb:.2f}\n\n"
+                 f"Modelo da Câmera: {model}\nFabricante: {make}\nData/Hora: {date_time}\n"
+                 f"Latitude: {lat}\nLongitude: {lon}")
 
 layout = [
     [sg.Menu([
             ['Arquivo', ['Abrir', 'Salvar', 'Fechar']],
-            ['Sobre a image', ['Informacoes']], 
+            ['Sobre a imagem', ['Informacoes']], 
             ['Sobre', ['Desenvolvedor']]
         ])],
     [sg.Image(key='-IMAGE-', size=(800, 600))],
 ]
 
-window = sg.Window('Aplicativo de image', layout, finalize=True)
+window = sg.Window('Aplicativo de Imagem', layout, finalize=True)
 
 while True:
     event, values = window.read()
@@ -59,12 +95,12 @@ while True:
     if event in (sg.WINDOW_CLOSED, 'Fechar'):
         break
     elif event == 'Abrir':
-        arquivo = sg.popup_get_file('Selecionar image', file_types=(("Imagens", "*.png;*.jpg;*.jpeg;*.gif"),))
+        arquivo = sg.popup_get_file('Selecionar imagem', file_types=(("Imagens", "*.png;*.jpg;*.jpeg;*.gif"),))
         if arquivo:
             open_image(arquivo)
     elif event == 'Salvar':
         if image_atual:
-            arquivo = sg.popup_get_file('Salvar image como', save_as=True, file_types=(("Imagens", "*.png;*.jpg;*.jpeg;*.gif"),))
+            arquivo = sg.popup_get_file('Salvar imagem como', save_as=True, file_types=(("Imagens", "*.png;*.jpg;*.jpeg;*.gif"),))
             if arquivo:
                 save_image(arquivo)
     elif event == 'Informacoes':
